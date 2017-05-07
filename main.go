c9f5dbb20ccc17f0d0230503370ad5f81c7c0d3b
nikuya3 <nikuya@dummy.mail>
2017-05-07 16:08:07 +0200

    Changed port

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Printf("Hello web")
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
 }

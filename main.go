a3a973693934f33bc0fd0280c207735d34c814fa
nikuya3 <nikuya@dummy.mail>
2017-05-08 21:34:14 +0200

    r

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "test")
-	/*cmd := exec.Command("Rscript", "pred/recognition.R", "pred/voices/erwin.wav")
+	cmd := exec.Command("Rscript", "pred/recognition.R", "pred/voices/erwin.wav")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 		fmt.Fprintf(w, "Internal server error")
 	}
-	fmt.Fprintf(w, out.String())*/
+	fmt.Fprintf(w, out.String())
 }
 
 func main() {

42f5cc7be3fd30f7642699df61852968e7a75e48
nikuya3 <nikuya@dummy.mail>
2017-05-08 20:43:52 +0200

    Set up server

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test %s", r.URL.Path[1:])
+	cmd := exec.Command("Rscript", "pred/recognition.R", "pred/voices/erwin.wav")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+		fmt.Fprintf(w, "Internal server error")
+	}
+	fmt.Fprintf(w, out.String())
 }
 
 func main() {
-	fmt.Printf("Hello web")
+	http.Handle("/", http.FileServer(http.Dir("./www/dist")))
 	http.HandleFunc("/recognize", handler)
 	fmt.Printf(os.Getenv("PORT"))
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
---
+++ pred/recognition.R
@@ -0,0 +1,66 @@
+library(fftw)
+library(seewave)
+library(tuneR)
+library(caret)
+
+humanFrequency <- 280
+
+analyzeWav <- function(file, start = 0, end = Inf) {
+  wave <- file
+  tuneWave <- readWave(file.path(getwd(), wave), from = start, to = end, units = "seconds")
+  waveSpec <- spec(tuneWave, f = tuneWave@samp.rate, plot = F)
+  analysis <- specprop(waveSpec, f = tuneWave@samp.rate, flim = c(0, humanFrequency / 1000), plot = F)
+  
+  meanfreq <- analysis$mean / 1000
+  sd <- analysis$sd / 1000
+  median <- analysis$median / 1000
+  Q25 <- analysis$Q25 / 1000
+  Q75 <- analysis$Q75 / 1000
+  IQR <- analysis$IQR / 1000
+  skew <- analysis$skewness
+  kurt <- analysis$kurtosis
+  sp.ent <- analysis$sh
+  sfm <- analysis$sfm
+  mode <- analysis$mode / 1000
+  centroid <- analysis$cent / 1000
+  
+  fundamental <- fund(tuneWave, f = tuneWave@samp.rate, ovlp = 50, threshold = 5, wl = 2048,
+                      ylim = c(0, humanFrequency / 1000), fmax = humanFrequency, plot = F)
+  
+  meanfun <- mean(fundamental[, 'y'], na.rm = T)
+  minfun <- min(fundamental[, 'y'], na.rm = T)
+  maxfun <- max(fundamental[, 'y'], na.rm = T)
+  
+  b <- c(0, 22)
+  dom <- dfreq(tuneWave, f = tuneWave@samp.rate, wl = 2048, ylim = c(0, humanFrequency / 1000),
+               ovlp = 0, threshold = 5, bandpass = b * 1000, fftw = T, plot = F)[, 2]
+  
+  meandom <- mean(dom, na.rm = TRUE)
+  mindom <- min(dom, na.rm = TRUE)
+  maxdom <- max(dom, na.rm = TRUE)
+  dfrange <- (maxdom - mindom)
+  duration <- (end - start)
+  
+  changes <- vector()
+  for(d in which(!is.na(dom))) {
+    change <- abs(dom[d] - dom[d + 1])
+    changes <- append(changes, change)
+  }
+  if(mindom == maxdom) modindx <- 0 else modindx <- mean(changes, na.rm = T) / dfrange
+  
+  obj <- data.frame(duration, meanfreq, sd, median, Q25, Q75, IQR, skew, kurt, sp.ent, sfm, mode, 
+                    centroid, meanfun, minfun, maxfun, meandom, mindom, maxdom, dfrange, modindx)
+  names(obj) <- c("duration", "meanfreq", "sd", "median", "Q25", "Q75", "IQR", "skew", "kurt", "sp.ent", 
+                  "sfm","mode", "centroid", "meanfun", "minfun", "maxfun", "meandom", "mindom", "maxdom",
+                  "dfrange", "modindx")
+  obj
+}
+
+path <- commandArgs(trailingOnly = T)
+analyzedVoice <- analyzeWav(path, end = 20)
+#control <- trainControl(method = "cv", number = 10)
+#model.forest <- train(label ~ ., data = file, method = "rf", metric = "Accuracy", trControl = control)
+model.forest <- readRDS('pred/model.forest.rds')
+prediction <- predict(model.forest, analyzedVoice)
+cat(prediction)
+cat(prediction, file = 'prediction.txt')

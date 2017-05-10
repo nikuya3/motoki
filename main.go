87d16ea2aecd861fd41df316b59b78381a679074
nikuya3 <nikuya@dummy.mail>
2017-05-10 19:59:20 +0200

    Fourth try

--- main.go
+++ main.go
@@ -30,8 +30,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Print(result)
 		fmt.Fprintf(w, "Internal server error")
 	}
-	log.Print(out.String())
-	fmt.Fprintf(w, out.String())
+
+	switch out.String() {
+	case "1":
+		fmt.Fprintf(w, "Female")
+
+	case "2":
+		fmt.Fprintf(w, "Male")
+	}
 }
 
 func main() {

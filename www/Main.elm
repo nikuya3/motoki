3e3fdc1f3e188ded0694c63958c6f8cfe1182cf5
nikuya3 <nikuya@dummy.mail>
2017-05-12 17:00:59 +0200

    Added title

--- www/Main.elm
+++ www/Main.elm
@@ -3,29 +3,40 @@ module Main exposing (..)
 import Html exposing (Html, button, div, program, text)
 import Html.Events exposing (onClick)
 
-type alias Model = 
+
+type alias Model =
     { test : String }
 
-type Msg = Test
+
+type Msg
+    = Test
+
 
 main : Program Never Model Msg
 main =
-    program { init = init, update = update, subscriptions = subscriptions, view = view } 
+    program { init = init, update = update, subscriptions = subscriptions, view = view }
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
     Model "" ! []
 
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-    Test ->
-        { model | test = "Changed" } ! []
+        Test ->
+            { model | test = "Changed" } ! []
+
 
 view : Model -> Html Msg
 view model =
-    div [] [ if String.isEmpty model.test then text "Hello world" else text model.test, button [ onClick Test ] [ text "Change" ] ]
+    div []
+        [ h1 [] [ text "Detect your sex - Using your voice" ]
+        , button [ id "startButton", onClick Test ] [ text "Start recording" ]
+        ]
